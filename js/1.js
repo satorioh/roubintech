7b6f0f698b9c430a16552cf1764ff77de20809b1
satorioh <wangbinxp@gmail.com>
2017-06-23 15:35:55 +0800

    add data funny column

--- js/1.js
+++ js/1.js
@@ -1,22 +1,47 @@
 /*popbox up & down function*/
 var $box = $('#_popBox');
 var $showFrame = $('#_showFrame');
-var $itemTitle = $('.itemTitle');
-var jTitle = document.getElementsByClassName('itemTitle');
+//var $itemTitle = $('.itemTitle');
+//var jTitle = document.getElementsByClassName('itemTitle');
 //console.log(Array.isArray(jTitle));
+var itemTitle = {
+    $itemTitle : $('.itemTitle'),
+    glyphiconToggle : function () {
+        //console.log(this);
+        //var $target = $(e.target);
+        var $span = $(this).children("span:nth-of-type(2)");
+        //console.log($span);
+        if ($span.hasClass("glyphicon-menu-up")){
+            $span.removeClass("glyphicon-menu-up").addClass("glyphicon-menu-down");
+            $(this).children('span.glyphicon-menu-down').parent().css("color","#fff");
+        }else {
+            $span.removeClass("glyphicon-menu-down").addClass("glyphicon-menu-up");
+            $(this).children('span.glyphicon-menu-up').parent().css("color","springgreen");
+        }
+    },
+    init : function () {
+        var $span = $(this).children("span:nth-of-type(2)");
+        //console.log($span);
+        if ($span.hasClass("glyphicon-menu-up")){
+            $span.removeClass("glyphicon-menu-up").addClass("glyphicon-menu-down");
+            $(this).children('span.glyphicon-menu-down').parent().css("color","#fff");
+        }
+    }
 
+};
 var popBox = {
     boxHeight : getComputedStyle($box[0]).height,
     boxWidth : getComputedStyle($box[0]).width,
     init : function () {
         console.log(this.boxHeight);
         console.log(this.boxWidth);
+        //console.log(itemTitle.$itemTitle);
         $box.css("marginBottom","-"+this.boxHeight);
     },
     down : function () {
         $box.animate({marginBottom:"-"+this.boxHeight,opacity:"0"},300,"swing");
-        $itemTitle.next().removeClass('in');
-        glyphiconToggle.bind(jTitle);
+        itemTitle.$itemTitle.next().removeClass('in');
+        itemTitle.init.call(itemTitle.$itemTitle);
 
     },
     up : function () {
@@ -37,22 +62,8 @@ $('#_showFrame').on("click",function (e) {
    console.log(e.clientY);
 });
 
-// var itemTitle = {
-//     $itemTitle : $('.itemTitle'),
-//
-// };
-function glyphiconToggle(e) {
-    var $span = $(this).children("span:last");
-    if ($span.hasClass("glyphicon-menu-down")){
-        $span.removeClass("glyphicon-menu-down").addClass("glyphicon-menu-up");
-        $(this).css("color","springgreen");
-    }else {
-        $span.removeClass("glyphicon-menu-up").addClass("glyphicon-menu-down");
-        $(this).css("color","#fff");
-    }
-}
 /*glyphicon-menu-down icon toggle*/
-$itemTitle.on("click", glyphiconToggle);
+itemTitle.$itemTitle.on("click", itemTitle.glyphiconToggle);
 
 // add jump out icon for link that has target attr
 $box.find("a.list-group-item[target]").append($('<span class="glyphicon glyphicon-share-alt pull-right"></span>'));
