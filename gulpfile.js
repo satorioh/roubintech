4de6575bc46d638f4c9de0f536cb9f5ae4398865
satorioh <wangbinxp@gmail.com>
2017-06-30 15:25:47 +0800

    use gulp to compress css/js

---
+++ gulpfile.js
@@ -0,0 +1,43 @@
+// 获取 gulp
+var gulp = require('gulp');
+// 获取 uglify 模块（用于压缩 JS）
+var uglify = require('gulp-uglify');
+// 获取 cleancss 模块（用于压缩 CSS）
+var cleanCSS = require('gulp-clean-css');
+var rename = require("gulp-rename");
+
+// 压缩 js 文件
+// 在命令行使用 gulp jscompress 启动此任务
+gulp.task('jscompress', function() {
+    // 1. 找到文件
+   return gulp.src('js/1.js')
+       .pipe(rename({suffix: '.min'}))
+    // 2. 压缩文件
+        .pipe(uglify())
+        // 3. 另存压缩后的文件
+        .pipe(gulp.dest('dist/js'));
+});
+// 压缩 css 文件
+// 在命令行使用 gulp csscompress 启动此任务
+gulp.task('csscompress', function() {
+    // 1. 找到文件
+   return gulp.src('css/my.css')
+        .pipe(rename({suffix: '.min'}))
+    // 2. 压缩文件
+        .pipe(cleanCSS())
+        // 3. 另存压缩后的文件
+        .pipe(gulp.dest('dist/css'));
+});
+
+// 在命令行使用 gulp auto 启动此任务
+gulp.task('auto', function () {
+    // 监听文件修改，当文件被修改则执行 script 任务
+    gulp.watch('js/1.js', ['jscompress']);
+    gulp.watch('css/my.css', ['csscompress']);
+});
+
+
+// 使用 gulp.task('default') 定义默认任务
+// 在命令行使用 gulp 启动 script 任务和 auto 任务
+gulp.task('default', ['auto']);
+
